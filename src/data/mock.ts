cd568e744ac04868c2ee1bf3f1f837b56adeb85a
Daniel Soledad <danielalsole24@gmail.com>
2026-05-04 11:37:31 +0200

    Create mock for the API

---
+++ src/data/mock.ts
@@ -0,0 +1,39 @@
+interface Product {
+    id: string;
+    name: string;
+    price: number;
+    stock: number;
+    is_active: boolean;
+    created_at: Date;
+    updated_at: Date;
+}
+
+export const mockProducts: Product[] = [
+    {
+        id: 'UD1',
+        name: 'Camiseta GitHub Originals',
+        price: 20,
+        stock: 10,
+        is_active: true,
+        created_at: new Date(),
+        updated_at: new Date(),
+    },
+    {
+        id: 'UD2',
+        name: 'Vaquero BluePrint ColdCulture',
+        price: 50,
+        stock: 8,
+        is_active: true,
+        created_at: new Date(),
+        updated_at: new Date(),
+    },
+    {
+        id: 'UD3',
+        name: 'Blusa Postman - U GET my URL',
+        price: 15,
+        stock: 6,
+        is_active: false,
+        created_at: new Date(),
+        updated_at: new Date(),
+    },
+];
